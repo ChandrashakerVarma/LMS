bd0b0bdcf340c8670c370c3e626ed00dddc22ce8
ravidasari00 <ravidasari269@gmail.com>
2025-12-30 13:58:17 +0530

    updated sub routes

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from app.routes import (auth_routes,role_routes,organization_routes,branch_route
     enrollment_routes,progress_routes,quiz_checkpoint_routes,quiz_history_routes,shift_routes,shift_change_request_routes,
     user_shifts_routes,shift_roster_routes,shift_roster_detail_routes,shift_summery_routes,attendance_punch_routes,
     leavemaster_routes,holiday_routes,permission_routes,salary_structure_routes,formula_routes,payroll_routes,
-    payroll_attendance_routes,job_posting_routes,job_description_routes,candidate_routes,candidates_documents_routes,
+    payroll_attendance_routes,job_posting_routes,job_description_routes,candidate_routes,candidates_documents_routes,subscription_routes,
     notification_routes,test_report_routes,attendance_summary_routes,leavetype_routes,leave_config_routes,leave_balance_routes)
 
 from app.routes.admin_dashboard import user_routes
@@ -39,6 +39,7 @@ app.include_router(branch_routes.router)
 app.include_router(menu_routes.router)
 app.include_router(role_right_routes.router)
 app.include_router(department_routes.router)
+app.include_router(subscription_routes.router)
 
 app.include_router(categorys_routes.router)
 app.include_router(course_routes.router)
